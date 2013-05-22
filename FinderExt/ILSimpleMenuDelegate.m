add5abea09c538e52fc73c5093cefc7cd0c61443
Ruchi Contractor <rcontractor@mezeo.com>
2013-05-22 17:50:57 -0500

    [MSYN-466]Only show context menu for objects in local sync dir
    In the integration to the new finder code, accidentally removed a chunk of code that restricted injecting the context menu to the local sync folder. Also do not show context menu if synchtml is set to 0 in config file.

--- FinderExt/ILSimpleMenuDelegate.m
+++ FinderExt/ILSimpleMenuDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "ILSimpleMenuDelegate.h"
+#import "FinderExt.h"
 
 @implementation ILSimpleMenuDelegate
 
@@ -28,6 +29,25 @@
   [super dealloc];
 }
 
+- (BOOL)isValidInsertion:(NSString *)fullPath
+{
+    // If sync.html is available on server, override options, otherwise do not
+    NSString *filePath = [NSString stringWithFormat: @"%@/Library/Application Support/%@/sync.cfg", NSHomeDirectory(), @BRAND];
+
+    NSString *jsonString = [[NSString alloc] initWithContentsOfFile:filePath encoding:NSUTF8StringEncoding error: NULL];
+
+    if ([jsonString rangeOfString:@"\"synchtml\": 1"].location == NSNotFound && [jsonString rangeOfString:@"\"synchtml\":1"].location == NSNotFound) {
+        return NO;
+    }
+    NSString *syncFolderName = [NSHomeDirectory() stringByAppendingPathComponent:@BRAND];
+
+    if (![fullPath hasPrefix:[NSString stringWithFormat:@"%@/", syncFolderName]] || [fullPath isEqualToString:syncFolderName]) {
+        return NO;
+    }
+
+    return YES;
+}
+
 - (void)finderWillShowContextMenu:(NSMenu *)menu
 {
     if ([_menuItemFile menu]) {
@@ -36,14 +56,29 @@
     }
 
     NSString *full_path = [[[ILFinderMenu sharedInstance] selectedItems] objectAtIndex:0];
+
+    if (![self isValidInsertion:full_path]) {
+        return;
+    }
+
+    // Find first separator to insert menu after it
+    NSInteger index;
+    for (index = 1 /* 0 is always separator  */; index < [menu numberOfItems]; ++index) {
+        if ([[menu itemAtIndex:index] isSeparatorItem]) {
+            // separator found!
+            break;
+        }
+    }
+
     // Show menu only for specified directory
     BOOL isDir;
-    
+
     if ([[NSFileManager defaultManager] fileExistsAtPath:full_path isDirectory:&isDir] && isDir) {
-        [menu insertItem:_menuItemFolder atIndex:_index];
+        [menu insertItem:_menuItemFolder atIndex:index + 1];
     } else {
-        [menu insertItem:_menuItemFile atIndex:_index];
+        [menu insertItem:_menuItemFile atIndex:index + 1];
     }
+    [menu insertItem:[NSMenuItem separatorItem] atIndex:index + 2];
 }
 
 @end
